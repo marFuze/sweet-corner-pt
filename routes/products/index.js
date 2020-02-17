7db3b1ee6e99748ee4508494dedd802c074ce35f
marFuze <marknar@hotmail.com>
2020-02-16 20:10:26 -0800

    updated single product details

--- routes/products/index.js
+++ routes/products/index.js
@@ -33,31 +33,65 @@ router.get('/', async (req, res, next) => {
 
 router.get('/full', async (req, res, next) => {
 
-    const sql = `select * from "products"`;
+    
 
-        const resp = await db.query(sql);
+    const sql = `select p."pid" as id, "caption", "cost", p."name", i."pid" as "tnId", "altText", "file", "type" from "products" as p left join "images" as i on p."id"=i."productId" where "type"='thumbnail';`
 
-        return productList = {
-            products: 'test'
-        }
+        const { rows: singleProduct } = await db.query(sql);
 
-        res.send(productList);
+        const formattedProductData = singleProduct.map( product => {
+            const  { tnId, altText, file, type, ...p} = product;
+ 
+            return {
+                ...p,
+                thumbnail: {
+                    id: tnId,
+                    altText: altText,
+                    file: file,
+                    type: type,
+                    url: `http://api.sc.lfzprototypes.com/images/thumbnails/${file}`
+                }
+            }
+         })
+
+        res.send(formattedProductData);
 
 });
 
 router.get('/:product', async (req, res, next) => {
 
-    const { product } = req.params;
+    const {product} = req.params;
+    
+    const sql = `select p."pid" as id, "caption", "cost", p."name", i."pid" as "tnId", "altText", "file", "type" from "products" as p left join "images" as i on p."id"=i."productId" where p."pid"=$1;`
 
-    const params = [ product ];
+        const { rows: singleProduct } = await db.query(sql,[product]);
+            const formattedSingleProduct = singleProduct.map(product => {
 
-    const sql = `select * from "products" where "pid" = $1`;
+            const { tnId, altText, file, type, ...p} = product;
+ 
+            return {
+                ...p,
+                image: {
+                    id: tnId,
+                    altText: altText,
+                    file: file,
+                    type: "full_images",
+                    url: `http://api.sc.lfzprototypes.com/images/full_images/${file}`
+                },
+                thumbnail: {
+                    id: tnId,
+                    altText: altText,
+                    file: file,
+                    type: "thumbnails",
+                    url: `http://api.sc.lfzprototypes.com/images/thumbnails/${file}`
+                }
+            }
 
-        const resp = await db.query(sql, params);
+            
 
-        res.send(resp.rows);
-        next(error);
-        // console.log('error', error);
+        });
+            const singleProductData = formattedSingleProduct[0];
+        res.send(singleProductData);
 });
 
 module.exports = router;
