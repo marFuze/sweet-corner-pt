9a1df886f5d5ee5712a53838d8cb7855437a8638
marFuze <marknar@hotmail.com>
2020-02-22 09:44:24 -0800

    query to get item added

--- routes/cart/index.js
+++ routes/cart/index.js
@@ -458,10 +458,93 @@ router.get('/totals', async (req, res, next) => {
     }
 })
 
-router.put('/items/:item_id', async (req, res, next) => {
+router.get('/items/:item_id', async (req, res, next) => {
     const authToken = req.headers.authorization
     const cartToken = req.headers['x-cart-token']
+    const { item_id } = req.params
     try {
+
+        console.log('item id', item_id)
+        if(authToken){
+            //decode auth token and get user pid, convert to user id
+            const decodedTokenData = jwt.decode(authToken,jwtSecret)
+            const {uid} = decodedTokenData
+            const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
+            const userId = getUserId.rows[0].id
+            //check for existing user cart and get related cartItems
+            const checkForUserCarts = await db.query('select "id","pid" from "carts" where "userId"=$1 and "statusId"=$2;',[userId, 2])
+            if(!checkForUserCarts){
+
+                res.status(404).send(
+                    {
+                        "cartId": null,
+                        "message": "No active cart"
+                    }
+                )
+                return
+            }
+            const userCartId = checkForUserCarts.rows[0].id
+            const userCartPid = checkForUserCarts.rows[0].pid
+            res.locals.cartId = userCartId
+            console.log("TCL: res.locals.cartId", res.locals.cartId)
+            res.locals.cartPid = userCartPid
+            console.log("TCL: res.locals.cartPid", res.locals.cartPid)
+            const getUserCartIdItems = await db.query(`select * from "cartItems" as ci join "products" as p on ci."productId"=p."id" join "images" as i on i."productId"=p."id" where "cartId"=$1 and ci."pid"=$2 and "type"=$3;`,[userCartId, item_id,'thumbnail'])
+            //console.log("TCL: getUserCartIdItems", getUserCartIdItems)
+            //console.log("TCL: getUserCartIdItems", getUserCartIdItems)
+            const getUserCartIdItemsResult = getUserCartIdItems.rows
+            console.log("TCL: getUserCartIdItemsResult", getUserCartIdItemsResult)
+            return
+
+            //get cart totals
+            const getCartTotals = await db.query(`select sum(cost) as totalCost, sum(quantity) as totalQuantity from "cartItems" as ci join "products" as p on ci."productId"=p."id" where "cartId"=$1 and "pid"=$2;`,[userCartId,item_id])
+            const getCartTotalsResult = getCartTotals.rows
+            const [{ totalcost, totalquantity}] = getCartTotalsResult
+
+             res.status(200).send(
+                {
+                     total: {
+                        cost: parseInt(totalcost),
+                        items: parseInt(totalquantity)
+                    }
+            })
+        }
+            if(cartToken){
+            const decodedToken = jwt.decode(cartToken, jwtSecret);
+            const {cartPid} = decodedToken;
+            res.locals.tokenCartPid = cartPid;
+            //convert cart pid to id
+            const getCartTokenCart = await db.query(`select * from "carts" where "pid"=$1`,[cartPid])
+            if(!getCartTokenCart){
+
+                res.status(404).send(
+                    {
+                        "cartId": null,
+                        "message": "No active cart"
+                    }
+                )
+                return
+            }
+            const cartTokenCartId = getCartTokenCart.rows[0].id
+            res.locals.cartId = cartTokenCartId
+            //get cart items
+            const getTokenCartIdItems = await db.query(`select * from "cartItems" as ci join "products" as p on ci."productId"=p."id" join "images" as i on i."productId"=p."id" where "cartId"=$1 and "type"=$2;`,[cartTokenCartId,'thumbnail'])
+            const getTokenCartIdItemsResult = getTokenCartIdItems.rows
+            //console.log("TCL: getUserCartIdItemsResult", getUserCartIdItemsResult)
+            //get cart totals
+            const getCartTotals = await db.query(`select sum(cost) as totalCost, sum(quantity) as totalQuantity from "cartItems" as ci join "products" as p on ci."productId"=p."id" where "cartId"=$1;`,[cartTokenCartId])
+            const getCartTotalsResult = getCartTotals.rows
+            const [{ totalcost, totalquantity}] = getCartTotalsResult
+           
+             res.status(200).send(
+                {
+                     total: {
+                        "cost": parseInt(totalcost),
+                        "items": parseInt(totalquantity)
+                    }
+            })
+        }
+    
 
     }
     catch(err){
