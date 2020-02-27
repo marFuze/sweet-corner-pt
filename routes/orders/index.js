2e98ab05339448cc143b6d51fdc08d67e1341933
marFuze <marknar@hotmail.com>
2020-02-26 18:31:54 -0800

    added base for get all orders, get order details, create new order

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -2,13 +2,63 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
+    const authToken = req.headers.authorization
 
-    const sql = `select * from "products"`;
+    try{
+    if(authToken){
+        //decode auth token and get user pid, convert to user id
+        const decodedTokenData = jwt.decode(authToken,jwtSecret)
+        const {uid} = decodedTokenData
+        const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
+        const userId = getUserId.rows[0].id
 
-        const resp = await db.query(sql);
+        const getOrders = await db.query(`select * from "orders" where "userId"=$1;`,[userId])
 
-        res.send(resp.rows);
+        const ordersResult = getOrders.rows[0]
+        console.log("TCL: ordersResult", ordersResult)
+        
+        res.send({message: 'from get all orders'});
+    }
+}
 
-});
+    catch(err){
+        next(err)
+    }
+})
+
+
+router.get('/:order_id', (req, res, next) => {
+    const authToken = req.headers.authorization
+
+    try{
+    if(authToken){
+        //decode auth token and get user pid, convert to user id
+        const decodedTokenData = jwt.decode(authToken,jwtSecret)
+        const {uid} = decodedTokenData
+        const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
+        const userId = getUserId.rows[0].id
+    }
+}
+catch(err){
+    next(err)
+}
+})
+
+router.post('/', (req, res, next) => {
+    const authToken = req.headers.authorization
+
+    try{
+    if(authToken){
+        //decode auth token and get user pid, convert to user id
+        const decodedTokenData = jwt.decode(authToken,jwtSecret)
+        const {uid} = decodedTokenData
+        const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
+        const userId = getUserId.rows[0].id
+    }
+}
+    catch(err){
+        next(err)
+    }
+})
 
 module.exports = router;
