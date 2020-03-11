ce7606d11ba9d836ec22e98596a38d69336bf638
marFuze <marknar@hotmail.com>
2020-03-10 19:22:47 -0700

    added email validation on create guest order

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -1,7 +1,3 @@
-// get guest order details
-//Email must be the same email the order was created with
-
-
 const express = require('express');
 const router = express.Router();
 const {db} = require('../../db');
@@ -9,6 +5,7 @@ const jwt = require('jwt-simple');
 const { jwtSecret } = require('../../config/jwt');
 const auth = require('../../middleware/auth');
 const {urlImages} = require('../../utility/url_images')
+const validator = require("email-validator")
 
 //get all user's orders
 
@@ -20,15 +17,12 @@ router.get('/', async (req, res, next) => {
         //decode auth token and get user pid, convert to user id
         const decodedTokenData = jwt.decode(authToken,jwtSecret)
         const {uid} = decodedTokenData
-        console.log("TCL: uid", uid)
         const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
         const userId = getUserId.rows[0].id
-        console.log("TCL: userId", userId)
 
         const getOrders = await db.query(`select "itemCount", "total", o."createdAt" as "orderCreatedAt", o."pid" as "orderPid", "name" as "orderStatusName" from "orders" as o join "orderStatuses" as os on o."statusId"=os."id" where "userId"=$1;`,[userId])
 
         const ordersResult = getOrders.rows
-        console.log("TCL: ordersResult", ordersResult)
         
         const userOrders = ordersResult.map(order => {
             const { itemCount, total, orderCreatedAt, orderPid, orderStatusName } = order
@@ -64,10 +58,8 @@ router.get('/:order_id', async (req, res, next) => {
         const {uid} = decodedTokenData
         const getUserId = await db.query(`select "id" from "users" where "pid"=$1;`,[uid])
         const userId = getUserId.rows[0].id
-        console.log("TCL: userId", userId)
 
         const getOrder = await db.query(`select * from "orders" where "pid"=$1 and "userId"=$2;`,[order_id,userId])
-        console.log("TCL: getOrder", getOrder)
         if(getOrder.rows == undefined || getOrder.rows.length == 0){
 
             res.status(404).send(
@@ -104,7 +96,6 @@ router.get('/:order_id', async (req, res, next) => {
             }
          })
 
-        //console.log("TCL: itemCount, pid, total, createdAt, statusId", itemCount, pid, total, createdAt, statusId)
         res.send({
             "itemCount": itemCount,
             "total": total,
@@ -138,7 +129,6 @@ router.post('/', async (req, res, next) => {
         const checkForUserCarts = await db.query('select "id" from "carts" where "userId"=$1 and "statusId"=$2;',[userId, 2])
         //console.log("TCL: checkForUserCarts", checkForUserCarts)
         const userCartId = checkForUserCarts.rows[0].id
-        console.log("TCL: userCartId", userCartId)
 
         //get cart items
         const getUserCartIdItems = await db.query(`select * from "cartItems" as ci join "products" as p on ci."productId"=p."id" join "images" as i on i."productId"=p."id" where "cartId"=$1 and "type"=$2;`,[userCartId,'thumbnail'])
@@ -146,7 +136,6 @@ router.post('/', async (req, res, next) => {
         //get cart totals
         const getCartTotals = await db.query(`select sum(cost) as totalCost, sum(quantity) as totalQuantity from "cartItems" as ci join "products" as p on ci."productId"=p."id" where "cartId"=$1;`,[userCartId])
         const getCartTotalsResult = getCartTotals.rows
-        console.log("TCL: getCartTotalsResult", getCartTotalsResult)
         const [{ totalcost, totalquantity}] = getCartTotalsResult
         //console.log("TCL: totalquantity", totalquantity)
     
@@ -154,13 +143,11 @@ router.post('/', async (req, res, next) => {
         const createUserOrder = await db.query(`insert into "orders" ("pid","itemCount", "total", "cartId", "guestId", "statusId","userId") values (uuid_generate_v4(),$1,$2,$3,$4,$5,$6) returning "id","pid";`,[totalquantity, totalcost, userCartId, 1, 1,userId])
         //console.log("TCL: createGuestOrder", createGuestOrder)
         const [{ id, pid }] = createUserOrder.rows
-        console.log("TCL: id,pid", id, pid)
         const newOrderId = id
         const newOrderPid = pid
         res.locals.newOrderPid = newOrderPid
         res.locals.newOrderId = newOrderId
-        console.log("TCL: res.locals.newOrderId", res.locals.newOrderId)
-        console.log("TCL: res.locals.newOrderPid", res.locals.newOrderPid)
+    
 
         const newOrderItems = getUserCartIdItemsResult.forEach (
             
@@ -174,8 +161,6 @@ router.post('/', async (req, res, next) => {
             next(err)
         }
      })
-
-
 
     console.log("TCL: newOrderItems", newOrderItems)
 
@@ -193,12 +178,16 @@ next(err)
 //create new guest order
 
 router.post('/guest', async (req, res, next) => {
-    console.log('create guest order triggered')
     const cartToken = req.headers['x-cart-token']
     const { email, firstName, lastName } = req.body
-    console.log("TCL: email", email)
     res.locals.guestEmail = email
     try{
+
+        if(!validator.validate(email)){
+            res.status(402).send('Please enter a valid email address.');
+                return;
+        }
+      
         if(cartToken){
             const decodedToken = jwt.decode(cartToken, jwtSecret);
             const {cartPid} = decodedToken;
@@ -216,10 +205,7 @@ router.post('/guest', async (req, res, next) => {
                 return
             }
             const cartTokenCartId = getCartTokenCart.rows[0].id
-            console.log("TCL: cartTokenCartId", cartTokenCartId)
             res.locals.cartId = cartTokenCartId
-            
-
 
             //get cart items
             const getTokenCartIdItems = await db.query(`select * from "cartItems" as ci join "products" as p on ci."productId"=p."id" join "images" as i on i."productId"=p."id" where "cartId"=$1 and "type"=$2;`,[cartTokenCartId,'thumbnail'])
@@ -227,40 +213,31 @@ router.post('/guest', async (req, res, next) => {
             //get cart totals
             const getCartTotals = await db.query(`select sum(cost) as totalCost, sum(quantity) as totalQuantity from "cartItems" as ci join "products" as p on ci."productId"=p."id" where "cartId"=$1;`,[cartTokenCartId])
             const getCartTotalsResult = getCartTotals.rows
-            console.log("TCL: getCartTotalsResult", getCartTotalsResult)
             const [{ totalcost, totalquantity}] = getCartTotalsResult
             //console.log("TCL: totalquantity", totalquantity)
 
             //create guest account if does not exist
 
             const checkGuestAccount = await db.query(`select "id" from "guests" where "email"=$1;`,[email])
-            console.log("checkGuestAccount", checkGuestAccount.rows[0])
 
             if(checkGuestAccount.rows.length==0){
             const createGuestAccount = await db.query(`insert into "guests" ("pid", "firstName", "lastName", "email") values (uuid_generate_v4(),$1,$2,$3) returning "id";`, [firstName, lastName, email])
-            console.log("createGuestAccount", createGuestAccount)
             const guestId  = createGuestAccount.rows[0].id
-            console.log("new guestId", guestId)
             res.locals.guestId = guestId
             }
 
             if(checkGuestAccount.rows.length>0){
             const existingGuestId = checkGuestAccount.rows[0].id
             res.locals.guestId = existingGuestId
-            console.log("res.locals.guestId", res.locals.guestId)
             }
         
             //insert new guest order
             const createGuestOrder = await db.query(`insert into "orders" ("pid","itemCount", "total", "cartId", "guestId", "statusId" ) values (uuid_generate_v4(),$1,$2,$3,$4,$5) returning "id","pid";`,[totalquantity, totalcost, cartTokenCartId, res.locals.guestId, 2])
-            //console.log("TCL: createGuestOrder", createGuestOrder)
             const [{ id, pid }] = createGuestOrder.rows
-            console.log("TCL: id,pid", id, pid)
             const newOrderId = id
             const newOrderPid = pid
             res.locals.newOrderPid = newOrderPid
             res.locals.newOrderId = newOrderId
-            console.log("TCL: res.locals.newOrderId", res.locals.newOrderId)
-            console.log("TCL: res.locals.newOrderPid", res.locals.newOrderPid)
 
             const newOrderItems = getTokenCartIdItemsResult.forEach (
                 
@@ -290,11 +267,8 @@ router.post('/guest', async (req, res, next) => {
 //Email must be the same email the order was created with
 
 router.get('/guest/:order_id', async (req, res, next) => { 
-    console.log('get guest order details triggered')
     const { email } = req.query
-    console.log(" email",  email)
     const { order_id } = req.params
-    console.log("TCL: order_id", order_id)
     try {
 
         const getOrder = await db.query(`select o."id" as "orderId", "itemCount", o."pid" as "orderPid", "total", o."createdAt"as "orderCreatedAt", "statusId"  from "orders" as o join "guests" as g on o."guestId"=g."id" where o."pid"=$1 and "email"=$2;`,[order_id, email])
